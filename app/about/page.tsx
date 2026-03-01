302d53d5db0d01e788630738e0f4ef9cd2c0267d
visualyuu <vegavega12365@gmail.com>
2026-03-01 03:53:34 -0600

    Mobile scaling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,10 +14,10 @@ export default function AboutPage(){
                 <div className="flex flex-row gap-8 w-full">
                 <Fade in = {true} {...(true ? { timeout: 1000 } : {})}>
                     <Box>
-                        <Typography variant="h6" sx={{ fontSize: '3rem' }}>
+                        <Typography variant="h6" sx={{ fontSize: { xs: '1.8rem', md: '3rem' } }}>
                             About ScootSafe
                         </Typography>
-                        <Typography sx={{ fontSize: '20px', p: "20px"}}>
+                        <Typography sx={{fontSize: { xs: '16px', md: '20px' }, p: { xs: '10px', md: '20px' }}}>
                             Chicagoans love the electric scooter but without the proper data we cannot ensure that we are doing enough to keep them safe on them. There are plenty of safety issues that can arise with the use of these scooters from, reckless driving, unsafe roads, and lack of knowledge on the condition of the roads. Many of these incidents go unreported. We have set out to fix this by collecting the data and compling it here capturing live reports of these issues. We hope that with this data the city can work towards helping these riders be safer.
                         </Typography>
                     </Box>
@@ -29,8 +29,8 @@ export default function AboutPage(){
             <div>
             <Fade in = {true} {...(true ? { timeout: 2000 } : {})}>
                 <Box sx={{ p: "20px"}}>
-                <Typography sx={{ fontSize: '1.5 rem' }}> -Ride safe,</Typography>
-                <Typography sx={{ fontSize: '2rem' }}>Depaul Demonhacks 2026 ScootSafe team!</Typography>
+                <Typography sx={{ fontSize: { xs: '1rem', md: '1.5rem' } }}> -Ride safe,</Typography>
+                <Typography sx={{ fontSize: { xs: '1.2rem', md: '2rem' } }}>Depaul Demonhacks 2026 ScootSafe team!</Typography>
                 </Box>
             </Fade>
             </div>
